e7a447975190992efcb76b54c75f962a4035f0fc
Precell <precell@uncommon.org>
2023-06-17 11:15:12 +0200

    added stack nav

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+
+const Stack = createNativeStackNavigator();
+
+const Home = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Home</Text>
     </View>
+  )
+}
+
+const Main = ({navigation}) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View >
+        <Text style={styles.title}>GAME ON</Text>
+      </View>
+      <View style={{width:300, height:200, backgroundColor:'#fff', flex:1,  justifyContent:'center', alignItems:'center'}}></View>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Home')}>
+        <Text style={styles.buttText}>Let's Begin</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +37,41 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title:{
+    fontSize:30,
+    fontWeight:'bold',
+    color:'#20315f',
+    marginTop:20
+  },
+  button:{
+    backgroundColor:'#ad40af',
+    padding:20,
+    width:'90%',
+    borderRadius:5,
+    flexDirection:'row',
+    justifyContent:'space-between',
+    marginBottom:50
+  },
+  buttText:{
+    fontWeight:'bold',
+    fontSize:18,
+color:'#fff'
+  },
 });
+
+export default App = () =>{
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          component={Main}
+          name='Main'
+        />
+        <Stack.Screen 
+          component={Home}
+          name='Home'
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+    )
+}
